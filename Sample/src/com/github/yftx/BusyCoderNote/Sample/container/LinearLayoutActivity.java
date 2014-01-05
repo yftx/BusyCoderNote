0aefefcab10dc10b1ed82d7ea80154e9f435f92b
yftx.net <yftx.net@gmail.com>
2014-01-05 13:11:32 +0800

    feature: LinearLayout weight propery description

---
+++ Sample/src/com/github/yftx/BusyCoderNote/Sample/container/LinearLayoutActivity.java
@@ -0,0 +1,15 @@
+package com.github.yftx.BusyCoderNote.Sample.container;
+
+import android.app.Activity;
+import android.os.Bundle;import com.github.yftx.BusyCoderNote.Sample.R;
+
+/**
+ * Created by yftx
+ * on 1/3/14.
+ */
+public class LinearLayoutActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linear_layout_activity);
+    }
+}
